a1eecf12beccf243142d65a0761e793fa50dde95
Tim Irmler <zwazel@hotmail.com>
2021-11-16 16:38:47 +0100

    FERTIG MIT EXPERT OF THE EXPERTS I AM OVERLORD

--- tim_stuff/ZahlenKonverter/src/main/java/ZahlenKonverterExperte/ZahlenKonverter.java
+++ tim_stuff/ZahlenKonverter/src/main/java/ZahlenKonverterExperte/ZahlenKonverter.java
@@ -6,28 +6,40 @@ import java.util.Scanner;
 
 public class ZahlenKonverter {
     Scanner scanner = new Scanner(System.in);
+    private final int MIN_BASE = 2;
+    private final int MAX_BASE = 36;
 
     public ZahlenKonverter() {
         int whatToDo = validInt("""
                 (1) Dezimal zu andere
                 (2) anderes zu Dezimal
+                (3) anderes zu anderes
                 >\s""");
 
         int number = 0;
         int numberSystem = 0;
+        String otherNumber = "";
         switch (whatToDo) {
             case 1 -> {
                 number = validInt("Bitte ihre Zahl eingeben: ");
-                numberSystem = validInt("Bitte ein Zahlensystem angeben (2-36): ");
+                numberSystem = validInt("Bitte ein Zahlensystem angeben (" + MIN_BASE + "-" + MAX_BASE + "): ");
                 System.out.println("Ihre Zahl: " + number);
                 System.out.println("Ihre Zahl mit Basis von " + numberSystem + ": " + convertToOtherBaseSystem(number, numberSystem));
             }
             case 2 -> {
                 System.out.print("Bitte ihre Zahl eingeben: ");
-                String otherNumber = scanner.nextLine();
-                numberSystem = validInt("Bitte das Zahlensystem der Zahl angeben (2-36): ");
+                otherNumber = scanner.nextLine();
+                numberSystem = validInt("Bitte das Zahlensystem der Zahl angeben (" + MIN_BASE + "-" + MAX_BASE + "): ");
                 System.out.println("Ihre Zahl: " + otherNumber + " mit Basis von " + numberSystem);
                 System.out.println("Ihre Zahl in Dezimal: " + convertToDecimal(otherNumber, numberSystem));
+            }
+            case 3 -> {
+                System.out.print("Bitte ihre Zahl eingeben: ");
+                otherNumber = scanner.nextLine();
+                numberSystem = validInt("Bitte das Zahlensystem der Zahl angeben (" + MIN_BASE + "-" + MAX_BASE + "): ");
+                int secondNumberSystem = validInt("Bitte das Zahlensystem angeben in der Sie ihre Zahl umwandeln wollen angeben (" + MIN_BASE + "-" + MAX_BASE + "): ");
+                System.out.println("Ihre Zahl: '" + otherNumber + "' mit Basis von '" + numberSystem + "'");
+                System.out.println("Ihre Zahl mit Basis von '" + secondNumberSystem + "': " + convertOtherToOther(otherNumber, numberSystem, secondNumberSystem));
             }
         }
     }
@@ -49,18 +61,23 @@ public class ZahlenKonverter {
         return number;
     }
 
+    public String convertOtherToOther(String num, int base1, int base2) {
+        int decimal = convertToDecimal(num, base1);
+        return convertToOtherBaseSystem(decimal, base2);
+    }
+
     public int convertToDecimal(String otherBaseNumber, int base) {
         // Source: https://www.tutorialspoint.com/computer_logical_organization/number_system_conversion.htm
 
         otherBaseNumber = otherBaseNumber.toUpperCase(Locale.ROOT);
         char[] chars = otherBaseNumber.toCharArray();
-        int baseCounter = otherBaseNumber.length()-1;
+        int baseCounter = otherBaseNumber.length() - 1;
         int result = 0;
         for (char c : chars) {
             int num = 0;
             int charToIntOffset = 49;
             int charToIntOffsetAdder = 1;
-            if (c > charToIntOffset) {
+            if (c > 57) {
                 charToIntOffset = 65;
                 charToIntOffsetAdder = 10;
             }
@@ -74,7 +91,9 @@ public class ZahlenKonverter {
 
     public String convertToOtherBaseSystem(int number, int numberSystem) {
         // Source: https://www.tutorialspoint.com/computer_logical_organization/number_system_conversion.htm
-        if (numberSystem < 2) {
+        if (numberSystem == 10) {
+            return "" + number;
+        } else if (numberSystem < 2) {
             numberSystem = 2;
         } else if (numberSystem > 36) {
             numberSystem = 36;
