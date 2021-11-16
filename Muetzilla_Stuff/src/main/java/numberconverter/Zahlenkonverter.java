d7e0131f0993f82ff54a6a64a7f183010a711050
Muetzilla <muetz-github@gasbichler.ch>
2021-11-16 11:06:33 +0100

    Boooom Expert

--- Muetzilla_Stuff/src/main/java/numberconverter/Zahlenkonverter.java
+++ Muetzilla_Stuff/src/main/java/numberconverter/Zahlenkonverter.java
@@ -11,17 +11,37 @@ public class Zahlenkonverter {
 
     public void interaction(){
         String input = "";
-        int numberToConvert = 0;
+        int numberToConvert;
+        int systemroot;
         do {
             System.out.print("Welche Zahl soll in ein anders System umgewandelt werden > ");
             input = scanner.nextLine();
         }while(!validInput(input));
         numberToConvert =  Integer.parseInt(input);
+
+        do {
+            System.out.print("In welches System soll diese Zahl umgewandelt werden > ");
+            input = scanner.nextLine();
+        }while(!validInput(input));
+        systemroot =  Integer.parseInt(input);
         System.out.println("In Bin with libary: " + Integer.toBinaryString(numberToConvert));
         System.out.println("In Hex with libary: " + Integer.toHexString(numberToConvert));
         System.out.println("In Bin Aufgabe 2: " + convertDezToBinary(numberToConvert));
         System.out.println("In Hex Aufgabe 2: " + convertDezToHex(numberToConvert));
+        System.out.println(numberToConvert + " ist im " + systemroot + "er System "  +convertDezToAny(numberToConvert,systemroot));
 
+    }
+
+    public String convertDezToAny(int dez, int systemRoot){
+        int tempInput = dez;
+        StringBuilder hexString = new StringBuilder();
+        while(tempInput > systemRoot){
+            int tempInt = tempInput % systemRoot;
+            hexString.append(checkLargerThenTen(tempInt));
+            tempInput /= systemRoot;
+        }
+        hexString.append(checkLargerThenTen(tempInput));
+        return String.valueOf(new StringBuilder(hexString).reverse());
     }
     public String convertDezToHex(int dez){
         int tempInput = dez;
@@ -38,7 +58,7 @@ public class Zahlenkonverter {
     public String checkLargerThenTen(int input){
         if(input >= 10){
             input -= 9;
-            return String.valueOf(Character.toChars(96 + input));
+            return String.valueOf(Character.toChars(64 + input));
         }else{
             return String.valueOf(input);
         }
--- Muetzilla_Stuff/src/test/java/numberconverter/ZahlenkonverterTest.java
+++ Muetzilla_Stuff/src/test/java/numberconverter/ZahlenkonverterTest.java
@@ -2,6 +2,8 @@ package numberconverter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZahlenkonverterTest {
@@ -18,16 +20,26 @@ class ZahlenkonverterTest {
     @Test
     public void TestHex7(){
         Zahlenkonverter zk = new Zahlenkonverter();
-        assertEquals(Integer.toHexString(7), zk.convertDezToHex(7));
+        assertEquals(Integer.toHexString(7), zk.convertDezToHex(7).toLowerCase(Locale.ROOT));
     }
     @Test
     public void TestHex21(){
         Zahlenkonverter zk = new Zahlenkonverter();
-        assertEquals(Integer.toHexString(21), zk.convertDezToHex(21));
+        assertEquals(Integer.toHexString(21), zk.convertDezToHex(21).toLowerCase(Locale.ROOT));
     }
     @Test
     public void TestHex174(){
         Zahlenkonverter zk = new Zahlenkonverter();
-        assertEquals(Integer.toHexString(174), zk.convertDezToHex(174));
+        assertEquals(Integer.toHexString(174), zk.convertDezToHex(174).toLowerCase(Locale.ROOT));
+    }
+    @Test
+    public void TestToAnyOctal(){
+        Zahlenkonverter zk = new Zahlenkonverter();
+        assertEquals(Integer.toOctalString(16), zk.convertDezToAny(16, 8).toLowerCase(Locale.ROOT));
+    }
+    @Test
+    public void TestToAny21erSystem(){
+        Zahlenkonverter zk = new Zahlenkonverter();
+        assertEquals("g42i", zk.convertDezToAny(150000, 21).toLowerCase(Locale.ROOT));
     }
 }
