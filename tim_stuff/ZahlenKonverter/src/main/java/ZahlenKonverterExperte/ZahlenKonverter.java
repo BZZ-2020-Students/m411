5c577236832b0aa3b3461b0e1c05c639abc03d4e
Tim Irmler <zwazel@hotmail.com>
2021-11-16 16:25:41 +0100

    expert lvl2!!

---
+++ tim_stuff/ZahlenKonverter/src/main/java/ZahlenKonverterExperte/ZahlenKonverter.java
@@ -0,0 +1,109 @@
+package ZahlenKonverterExperte;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ZahlenKonverter {
+    Scanner scanner = new Scanner(System.in);
+
+    public ZahlenKonverter() {
+        int whatToDo = validInt("""
+                (1) Dezimal zu andere
+                (2) anderes zu Dezimal
+                >\s""");
+
+        int number = 0;
+        int numberSystem = 0;
+        switch (whatToDo) {
+            case 1 -> {
+                number = validInt("Bitte ihre Zahl eingeben: ");
+                numberSystem = validInt("Bitte ein Zahlensystem angeben (2-36): ");
+                System.out.println("Ihre Zahl: " + number);
+                System.out.println("Ihre Zahl mit Basis von " + numberSystem + ": " + convertToOtherBaseSystem(number, numberSystem));
+            }
+            case 2 -> {
+                System.out.print("Bitte ihre Zahl eingeben: ");
+                String otherNumber = scanner.nextLine();
+                numberSystem = validInt("Bitte das Zahlensystem der Zahl angeben (2-36): ");
+                System.out.println("Ihre Zahl: " + otherNumber + " mit Basis von " + numberSystem);
+                System.out.println("Ihre Zahl in Dezimal: " + convertToDecimal(otherNumber, numberSystem));
+            }
+        }
+    }
+
+    public int validInt(String message) {
+        boolean validNumber = false;
+        int number = -1;
+        while (!validNumber) {
+            System.out.print(message);
+            String numberString = scanner.nextLine();
+
+            try {
+                number = Integer.parseInt(numberString);
+                validNumber = true;
+            } catch (NumberFormatException e) {
+                System.err.println("Dies ist keine gültige Zahl!");
+            }
+        }
+        return number;
+    }
+
+    public int convertToDecimal(String otherBaseNumber, int base) {
+        // Source: https://www.tutorialspoint.com/computer_logical_organization/number_system_conversion.htm
+
+        otherBaseNumber = otherBaseNumber.toUpperCase(Locale.ROOT);
+        char[] chars = otherBaseNumber.toCharArray();
+        int baseCounter = otherBaseNumber.length()-1;
+        int result = 0;
+        for (char c : chars) {
+            int num = 0;
+            int charToIntOffset = 49;
+            int charToIntOffsetAdder = 1;
+            if (c > charToIntOffset) {
+                charToIntOffset = 65;
+                charToIntOffsetAdder = 10;
+            }
+
+            num = (int) c - charToIntOffset + charToIntOffsetAdder;
+            result += (num * Math.pow(base, baseCounter--));
+        }
+
+        return result;
+    }
+
+    public String convertToOtherBaseSystem(int number, int numberSystem) {
+        // Source: https://www.tutorialspoint.com/computer_logical_organization/number_system_conversion.htm
+        if (numberSystem < 2) {
+            numberSystem = 2;
+        } else if (numberSystem > 36) {
+            numberSystem = 36;
+        }
+        int result = number / numberSystem;
+        int remainder = number % numberSystem;
+        StringBuilder newNumber = new StringBuilder();
+        ArrayList<Integer> newNumbers = new ArrayList<>();
+        newNumbers.add(remainder);
+        while (result != 0) {
+            remainder = result % numberSystem;
+            result /= numberSystem;
+            newNumbers.add(remainder);
+        }
+
+        for (int i = newNumbers.size() - 1; i >= 0; i--) {
+            int num = newNumbers.get(i);
+            String numString = "" + num;
+            if (num > 9) {
+                numString = "" + (char) (65 + (num - 10));
+            }
+
+            newNumber.append(numString);
+        }
+
+        return newNumber.toString();
+    }
+
+    public static void main(String[] args) {
+        new ZahlenKonverter();
+    }
+}
