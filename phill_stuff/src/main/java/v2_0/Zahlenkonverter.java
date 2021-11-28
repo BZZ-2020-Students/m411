81af7746624ab12b83b2f6b474b38483b4c13c66
Phillip <phillip.jerebic@gmail.com>
2021-11-28 16:48:33 +0100

    ich checke nix:)

--- phill_stuff/src/main/java/v2_0/Zahlenkonverter.java
+++ phill_stuff/src/main/java/v2_0/Zahlenkonverter.java
@@ -1,8 +1,14 @@
 package v2_0;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Integer;
+
 public class Zahlenkonverter {
     private Einleser einleser;
     private String natuerlichezahl = "";
+    private String divider = "";
     private String umgewandelteNummerHex = "";
     private String umgewandelteNummerBIN = "";
 
@@ -10,15 +16,67 @@ public class Zahlenkonverter {
         einleser = new Einleser();
     }
 
-    public String Usereingaben(){
+    public void Usereingaben(){
         System.out.println("Wilkommen beim Zahlenkonverter");
         System.out.println("Geben Sie eine Zahl ein");
         natuerlichezahl = einleser.readString();
-        return natuerlichezahl;
+        System.out.println("Wie soll umgewandelt werden? [BIN = 2 | HEX = 16]");
+        divider = einleser.readString();
+        if (divider.equals("2")){
+            bin();
+        }else if(divider.equals("16")){
+            hex();
+        }
     }
 
-    public void convert(String zuconverten){
+    public void bin(){
+        System.out.println("BIN");
+        StringBuilder result = new StringBuilder();
+        float interimresult = Float.parseFloat(natuerlichezahl);
+        float dividerF = Float.parseFloat(divider);
+        List<Integer> remainder = new ArrayList<Integer>();
+        do{
+            interimresult = interimresult / dividerF;
+            if (interimresult % 1 == 0){
+                remainder.add(0);
+            }else{
+                remainder.add(1);
+            }
+            interimresult = (int)interimresult;
+        }while (interimresult != 0);
 
+
+        for (int i = remainder.size() - 1; i >= 0; i--) {
+            result.append(Integer.toString(remainder.get(i)));
+        }
+        umgewandelteNummerBIN = result.toString();
+        umgewandelteNummerHex = "-";
+    }
+
+    public void hex(){
+        System.out.println("HEX");
+        StringBuilder result = new StringBuilder();
+        float interimresultint = Float.parseFloat(natuerlichezahl);
+        float dividerF = Float.parseFloat(divider);
+        List<Integer> remainder = new ArrayList<Integer>();
+
+        do{
+            float interimresult = interimresultint / dividerF;
+            remainder.add((int)((interimresult - Integer.valueOf((int) interimresult).floatValue())*16));
+            interimresultint = (int)interimresult;
+        }while (interimresultint != 0);
+
+
+        for (int i = remainder.size() - 1; i >= 0; i--){
+
+            if (remainder.get(i) > 9) {
+                result.append((char) (65 + (remainder.get(i) - 10)));
+            }else{
+                result.append(remainder.get(i));
+            }
+        }
+        umgewandelteNummerBIN = "-";
+        umgewandelteNummerHex = result.toString();
     }
 
     public void printAllNumbers(){
--- phill_stuff/src/main/java/v2_0/ZahlenkonverterApp.java
+++ phill_stuff/src/main/java/v2_0/ZahlenkonverterApp.java
@@ -2,11 +2,8 @@ package v2_0;
 
 public class ZahlenkonverterApp {
     public static void main(String[] args) {
-        String eingegebeneZahl;
         Zahlenkonverter konverter = new Zahlenkonverter();
-        eingegebeneZahl = konverter.Usereingaben();
- 
-        konverter.convert(eingegebeneZahl);
+        konverter.Usereingaben();
         konverter.printAllNumbers();
     }
 }
