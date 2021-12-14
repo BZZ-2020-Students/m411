2285a7d096c1040bcde868f6dcdfd579a07a5998
Muetzilla <muetz-github@gasbichler.ch>
2021-12-14 09:23:40 +0100

    rand pivot

--- Muetzilla_Stuff/src/main/java/quicksort/Quicksort.java
+++ Muetzilla_Stuff/src/main/java/quicksort/Quicksort.java
@@ -1,4 +1,16 @@
 package quicksort;
 
+import java.util.Random;
+
 public class Quicksort {
+    private int []arrayToSort = {4,7,2,8,9,6,1,3};
+    public static void main(String[] args) {
+        Quicksort quicksort = new Quicksort();
+    }
+    public Quicksort(){
+        int pivot = arrayToSort[new Random().nextInt(arrayToSort.length)];
+
+    }
+
+
 }
